ce3ded7595e4a40fa9f747d3b4db438eebae77b5
reyclouddev-ops <reyclouddev@gmail.com>
2026-07-19 19:24:38 +0700

    Update auth.js

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,7 +1,12 @@
 const router = require("express").Router();
 
 const AuthController = require("../controllers/authController");
+const auth = require("../middleware/auth");
 
 router.post("/register", AuthController.register);
+
+router.post("/login", AuthController.login);
+
+router.get("/profile", auth, AuthController.profile);
 
 module.exports = router;
