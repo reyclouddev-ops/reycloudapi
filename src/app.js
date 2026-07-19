89848f59e4d48f054704c882c71ba498bb50c0f5
reyclouddev-ops <reyclouddev@gmail.com>
2026-07-19 18:59:59 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const morgan = require("morgan");
 
 const Response = require("./utils/response");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 
@@ -10,13 +11,15 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
+app.use("/api", authRoutes);
+
 app.get("/", (req, res) => {
 
     Response.success(res, "ReyCloudAPI Online", {
 
         name: process.env.APP_NAME,
 
-        version: "1.0.0"
+        version: process.env.APP_VERSION
 
     });
 
